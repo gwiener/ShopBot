ab5063d172353c4852a381061e1be7d5ba26ece8
Guy Wiener <guy.wiener@hpe.com>
2016-08-18 23:13:50 +0300

    Revert to WinPython 2, Scipy's one-hot encoder and tf-idf seems to work.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-ShopBot
+# ShopBot
+
+## Prerequisits
+- scikit, that requires
+- scipy, that requires
+- numpy+mkl
+- pandas
+
+For Windows I am trying to use 
+[WinPython-3.5.1 64bit no Qt](https://sourceforge.net/projects/winpython/files/WinPython_3.5/3.5.2.1/),
+after trying to install scipy from pip failed miserably.
+
+WinPython 3 also failed, reverting to WinPython-2.7.10
--- experiment.py
+++ experiment.py
@@ -3,12 +3,18 @@ Experiment file
 """
 
 import json
-
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 class Experiment(object):
     def __init__(self):
         self.pros_cons = {}
         self.labels = {}
+        self.defaults = {
+            "size": 0, "weight": 0, "resolution": 0, "display": 0, "pixel_density": 0,
+            "camera": 0, "cpu": 0, "ram": 0, "battery": 0, "data": 0
+        }
+        self.enc_labels = []
 
     def load_pros_cons(self, path):
         with open(path) as f:
@@ -20,11 +26,26 @@ class Experiment(object):
 
     def load_labels(self, path):
         with open(path) as f:
-            self.labels = json.load(f)
+            labels = json.load(f)
+        for phone, attrs in labels.items():
+            bats = [v for k, v in attrs.items() if k.startswith('battery')]
+            battery = (bats and int(sum(bats) / len(bats))) or 1
+            new_attrs = {k: v for k, v in attrs.items() if not k.startswith('battery')}
+            new_attrs['battery'] = battery
+            self.labels[phone] = self.defaults.copy()
+            self.labels[phone].update(new_attrs)
+
+    def encode_labels(self):
+        ohe = OneHotEncoder()
+        labels = [l.values() for k, l in self.labels.items()]
+        ohe.fit(labels)
+        self.enc_labels = ohe.transform(labels)
 
     def run(self):
         self.load_pros_cons('proscons.csv')
         self.load_labels('labels.json')
+        self.encode_labels()
+        print self.enc_labels.toarray()[:10, :]
 
 if __name__ == '__main__':
     Experiment().run()
