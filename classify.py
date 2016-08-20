2a9cea3388d10bde42f020c1355f31fd43f921a7
Guy Wiener <guy.wiener@hpe.com>
2016-08-20 21:53:01 +0300

    Add feature importance print-outs during training

--- classify.py
+++ classify.py
@@ -10,16 +10,24 @@ class Detector(object):
     def __init__(self, column):
         self.column = column
         self.vec = CountVectorizer(stop_words='english', max_features=100)
-        self.clf = SGDClassifier()
+        # self.clf = SGDClassifier()
+        self.clf = DecisionTreeClassifier()
 
     def fit(self, df):
         data = df[['text', self.column]].dropna()
+        print('training %s on %d samples' % (self.column, len(data)))
         text = data['text']
         col = data[self.column]
         x = self.vec.fit_transform(text)
-        ql = col.quantile([0.2, 0.8])
-        y = ~col.between(*ql)
+        # ql = col.quantile([0.2, 0.8])
+        ql = col.quantile(0.2)
+        # y = ~col.between(*ql)
+        y = col <= ql
         self.clf.fit(x, y)
+        weights = self.clf.feature_importances_
+        args = reversed(weights.argsort()[-10:])
+        names = self.vec.get_feature_names()
+        print(self.column, [(names[arg], '%.2f' % weights[arg]) for arg in args])
 
     def predict(self, queries):
         q = self.vec.transform(queries)
