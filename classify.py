2018f86581cdf996e5abc19e0fa96867041301fb
Guy Wiener <guy.wiener@hpe.com>
2016-08-20 21:19:30 +0300

    Generalize classification script but results are poor

--- classify.py
+++ classify.py
@@ -2,27 +2,50 @@ import pandas as pd
 from prep_text import load_pros_cons
 from sys import argv
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.cross_validation import cross_val_score
 from sklearn.linear_model import SGDClassifier
 from sklearn.feature_extraction.text import CountVectorizer
 
-text = load_pros_cons(argv[1])
-df = pd.read_csv(argv[2], index_col=[0])
-df['text'] = text
-data = df[['text', 'weight']].dropna()
-ql = data.weight.quantile([0.2, 0.8])
-y = data.weight.between(*ql)
-print(y.value_counts())
-vec_ = CountVectorizer(stop_words='english', max_features=50)
-x = vec_.fit_transform(data['text'])
-# clf = DecisionTreeClassifier()
-clf = SGDClassifier()
-score = cross_val_score(clf, x, y)
-print(score)
 
-clf.fit(x, y)
-queries = ["I want a lightweight phone", "the camera is not good", "phone too heavy", "weak battery", "thin phone"]
-q = vec_.transform(queries)
-print(clf.predict(q))
-# print(clf.predict_proba(q))
+class Detector(object):
+    def __init__(self, column):
+        self.column = column
+        self.vec = CountVectorizer(stop_words='english', max_features=100)
+        self.clf = SGDClassifier()
 
+    def fit(self, df):
+        data = df[['text', self.column]].dropna()
+        text = data['text']
+        col = data[self.column]
+        x = self.vec.fit_transform(text)
+        ql = col.quantile([0.2, 0.8])
+        y = ~col.between(*ql)
+        self.clf.fit(x, y)
+
+    def predict(self, queries):
+        q = self.vec.transform(queries)
+        return self.clf.predict(q)
+
+
+def main():
+    text = load_pros_cons(argv[1])
+    df = pd.read_csv(argv[2], index_col=[0])
+    df['text'] = text
+    detectors = [Detector('weight'), Detector('thickness'), Detector('memory'), Detector('standby_time')]
+    for det in detectors:
+        det.fit(df)
+    queries = [
+        "I want a lightweight phone",
+        "the camera is not good",
+        "phone too heavy",
+        "weak battery",
+        "thin phone",
+        "not enough ram"
+    ]
+    results = {det.column: det.predict(queries) for det in detectors}
+    for name, res in results.items():
+        print(name)
+        for q, p in zip(queries, res):
+            print(q, p)
+
+if __name__ == '__main__':
+    main()
